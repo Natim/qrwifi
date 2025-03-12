bfb84fa62a80b2096e4fcf0d7fb2a4ce87216c3e
Rémy Hubscher <remy@yampa.ai>
2025-03-12 18:20:41 +0100

    Add new QRCode URL encoder

--- qrwifi/qrwifi.py
+++ qrwifi/qrwifi.py
@@ -6,10 +6,33 @@ import base64
 
 app = Flask(__name__)
 
+import qrcode
+
+
+def png_base64_image(content):
+    img = qrcode.make(content)
+
+    with BytesIO() as f:
+        img.save(f, "PNG", quality=80)
+        imgb64 = base64.b64encode(f.getvalue()).decode('utf-8')
+
+    return imgb64
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/url/")
+def url():
+    return render_template('url.html')
+
+@app.route("/url/", methods=['POST'])
+def url_qrcode():
+    url = request.form.get("url")
+
+    imgb64 = png_base64_image(url)
+    return render_template('qrimage.html', imgb64=imgb64, url=url)
 
 
 @app.route("/qrwifi/", methods=['POST'])
@@ -26,14 +49,8 @@ def qrwifi():
     else:
         stype = 'WEP'
 
-    img = encode_to_img(("WIFI:S:{};T:{};P:{};;".format(ssid, stype, passwd)).encode('utf-8'),
-                        500, 20)
-    f = BytesIO()
-    img.save(f, "PNG", quality=80)
-    imgb64 = base64.b64encode(f.getvalue()).decode('utf-8')
-    f.close()
-
-    return render_template('qrimage.html', imgb64=imgb64, ssid=ssid)
+    imgb64 = png_base64_image("WIFI:S:{};T:{};P:{};;".format(ssid, stype, passwd))
+    return render_template('qrimage-wifi.html', imgb64=imgb64, ssid=ssid)
 
 if __name__ == "__main__":
     app.run(debug=True)
